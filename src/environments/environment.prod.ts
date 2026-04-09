c54717a2603028d14a3f1c0853d326175501cb26
chirag.chauhan <chirag.chauhan@promptdairytech.in>
2026-04-09 20:08:14 +0530

    change backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://jobapplication-e5h7e5g2dua5d0a8.centralindia-01.azurewebsites.net/api'
+  //apiUrl: 'https://jobapplication-e5h7e5g2dua5d0a8.centralindia-01.azurewebsites.net/api'
+  apiUrl: 'https://easyapplyapi2-a4gvdpfke5h9ghg7.canadacentral-01.azurewebsites.net/api'
 };
